c5a48cdd5a6e7d1f7aee489e4da2195ca441ffa8
timger <yishenggudou@gmail.com>
2013-04-07 15:26:51 +0800

    modified:   weixin_en2zh/weixin.py

--- weixin_en2zh/weixin.py
+++ weixin_en2zh/weixin.py
@@ -42,24 +42,31 @@ class MainHandler(tornado.web.RequestHandler):
         查询google翻译后返回结果
         """
         recv = xmltodict.parse(self.request.body)['xml']
-        text = recv['Content'].strip()
-        if recv.get('Event') == 'subscribe':
-            hellotext = u"""欢迎关注en2zh机器人,
-                            en2zh是一个自动翻译的机器人,
-                            你输入的任何英文en2zh将会为你翻译成中文"""
+        hellotext = u"""欢迎关注en2zh机器人,
+                        en2zh是一个自动翻译的机器人,
+                        你输入的任何英文en2zh将会为你翻译成中文"""
+        try:
+            if recv.get('Event') == 'subscribe':
+                text_reply = reply_text_user(recv['ToUserName'],
+                                             recv['FromUserName'],
+                                             recv['CreateTime'],
+                                             'text',
+                                             hellotext)
+
+            else:
+                text = recv['Content'].strip()
+                en2zh = translate.en2zh(text)
+                text_reply = reply_text_user(recv['ToUserName'],
+                                             recv['FromUserName'],
+                                             recv['CreateTime'],
+                                             'text',
+                                             en2zh)
+        except:
             text_reply = reply_text_user(recv['ToUserName'],
                                          recv['FromUserName'],
                                          recv['CreateTime'],
                                          'text',
                                          hellotext)
-
-        else:
-            en2zh = translate.en2zh(text)
-            text_reply = reply_text_user(recv['ToUserName'],
-                                         recv['FromUserName'],
-                                         recv['CreateTime'],
-                                         'text',
-                                         en2zh)
         self.write(text_reply)
         return
 
